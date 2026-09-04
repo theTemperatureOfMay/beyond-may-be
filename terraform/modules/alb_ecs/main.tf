503d6541f202de24e9825dad81c3808dcb32e7ca
hyejin <hyejin66@sookmyung.ac.kr>
2026-09-04 15:14:03 +0900

    chore : ECS 컨테이너에 AWS_REGION 환경변수 추가
    S3 SDK가 컨테이너 안에서 리전을 자동으로 못 찾는 경우를 대비해 명시적으로 주입한다.

--- terraform/modules/alb_ecs/main.tf
+++ terraform/modules/alb_ecs/main.tf
@@ -150,6 +150,7 @@ resource "aws_ecs_task_definition" "this" {
         { name = "TOURISM_API_BASE_URL", value = var.tourism_api_base_url },
         { name = "TOURISM_POPULARITY_API_BASE_URL", value = var.tourism_popularity_api_base_url },
         { name = "S3_BUCKET_NAME", value = var.s3_bucket_name },
+        { name = "AWS_REGION", value = var.aws_region },
       ]
       secrets = [
         { name = "DB_URL", valueFrom = var.db_url_parameter_arn },
